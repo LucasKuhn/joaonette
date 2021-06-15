46d0ca3f03bc6f777190be98d34e33ac870f769a
joalbert <joalbert@student.42sp.org.br>
2021-06-15 18:22:32 +0000

    fix c03 ex02

--- C03/ex02/main.c
+++ C03/ex02/main.c
@@ -6,9 +6,12 @@ char	*ft_strcat(char *dest, char *src);
 
 int	main(void)
 {
-
 	char dest[10] = "123";
 	char src[] = "456";
 	printf("src: %s, dest: %s\n", src, dest);
-	printf("ft_strcat = %s, strcat = %s\n", ft_strcat(dest, src), strcat(dest, src));
+	printf("strcat = %s\n", strcat(dest, src));
+
+	char dest2[10] = "123";
+	char src2[] = "456";
+	printf("ft_strcat = %s\n", ft_strcat(dest2, src2));
 }
