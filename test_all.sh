40ea6222bc8dd1d512ca9352588c66b8803d29c2
lalex-ku <lalex-ku@student.42sp.org.br>
2021-06-16 14:43:29 +0000

    add colors

--- test_all.sh
+++ test_all.sh
@@ -1,14 +1,18 @@
 LIST="${LIST:-$(basename ${PWD})}"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 for d in */ ; do
     echo "===== $d ====="
 	cc -Wall -Wextra -Werror -lbsd ~/tests/$LIST/$d/*.c $d/*.c -o $d/a.out
 	DIFF=$(diff <($d/a.out) ~/tests/$LIST/$d/expected_output)
 	if [[ $DIFF ]]; then
-		diff <($d/a.out) ~/tests/$LIST/$d/expected_output | cat -e
-		echo "KO"
+		# diff <($d/a.out) ~/tests/$LIST/$d/expected_output | cat -e
+		echo -e "KO ${RED}✖${NC}"
 	else
-		echo "OK"
+		echo -e "OK ${GREEN}✓${NC}"
 	fi
 done
 
