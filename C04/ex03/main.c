be05a38dfbe531bf94d06775467ba2f833034a8d
lalex-ku <lalex-ku@student.42sp.org.br>
2021-06-17 23:12:43 +0000

    update C04ex03

--- C04/ex03/main.c
+++ C04/ex03/main.c
@@ -9,10 +9,14 @@ int		main(void)
 	char str3[] = "++++42";
 	char str4[] = "-++-42";
 	char str5[] = " ---01";
+	char str6[] = "-  123";
+	char str7[] = "";
 
 	printf("str: %s | ft_atoi: %d\n", str1, ft_atoi(str1));
 	printf("str: %s | ft_atoi: %d\n", str2, ft_atoi(str2));
 	printf("str: %s | ft_atoi: %d\n", str3, ft_atoi(str3));
 	printf("str: %s | ft_atoi: %d\n", str4, ft_atoi(str4));	
 	printf("str: %s | ft_atoi: %d\n", str5, ft_atoi(str5));	
+	printf("str: %6s | ft_atoi: %d\n", str6, ft_atoi(str6));	
+	printf("str: %6s | ft_atoi: %d\n", str7, ft_atoi(str7));	
 }
