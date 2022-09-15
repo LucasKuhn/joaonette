2ac1f7a914645753e2ea1a92c5cd9cd51413c55f
Lucas Kuhn <lucasalexkuhn@gmail.com>
2022-09-15 16:52:13 +0200

    simplifies script

--- C00/ex07/main.c
+++ C00/ex07/main.c
@@ -12,4 +12,7 @@ int	main(void)
 	ft_putnbr(-2147483648);
 	write(1,"\n",1);
 	ft_putnbr(42);
+	write(1,"\n",1);
+	ft_putnbr(-42);
+	write(1,"\n",1);
 }
--- joaonette.sh
+++ joaonette.sh
@@ -4,8 +4,8 @@ NC='\033[0m'
 
 check_exercise()
 {
-	cc -Wall -Wextra -Werror -lbsd ~/joaonette/$LIST/$DIR/*.c $LOCAL/*.c -o $LOCAL/a.out || ERROR=true
-	DIFF=$(diff <($LOCAL/a.out) ~/joaonette/$LIST/$DIR/expected_output) || ERROR=true
+	cc -Wall -Wextra -Werror -lbsd $BASEDIR/$LIST/$DIR/*.c $LOCAL/*.c -o $LOCAL/a.out || ERROR=true
+	DIFF=$(diff <($LOCAL/a.out) $BASEDIR/$LIST/$DIR/expected_output) || ERROR=true
 	rm $LOCAL/a.out
 	if [[ "$DIFF" == "" && "$ERROR" == "" ]]; then
 		echo -e "OK ${GREEN}✓${NC}"
@@ -13,23 +13,35 @@ check_exercise()
 		echo -e "KO ${RED}✖${NC}"
 	fi
 
-	if [[ -n "$DIFF" && "$CHECKING_LIST" == "" ]]; then
+	if [[ -n "$DIFF" ]]; then
 		echo -e "----- DIFF ----- ( < user | > expected )"
-		diff <($LOCAL/a.out) ~/joaonette/$LIST/$DIR/expected_output
+		diff <($LOCAL/a.out) $BASEDIR/$LIST/$DIR/expected_output
+	fi
+
+	if [ "$UPDATE" = "true" ] ; then
+		echo -e "\nUpdating expected_output..."
+		$d/a.out > ~/joaonette/$LIST/$d/expected_output 
 	fi
 }
 
-# If you are inside an exercise folder
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
+
+# If you are inside an exercise folder (starting with ex)
+# check the current exercise
 if [[ $(basename $PWD) == ex* ]]; then
 	LIST=$(basename $(dirname $PWD))
 	DIR=$(basename $PWD)
 	LOCAL=$(echo $PWD)
 	check_exercise
 
-# If you are not in an exercise folder (presumably in a list folder)
+# If you are not in an exercise folder corrects the entire list
+# either gets the list name from LIST variable
+# or assumes based on folder name (ex: C00)
 else
-	LIST=$(basename ${PWD})
-	CHECKING_LIST="true"
+	LIST="${LIST:-$(basename ${PWD})}"
+	echo -e "\n===== NORMINETTE ====="
+	norminette -R CheckForbiddenSourceHeader
 	for DIR in */ ; do
 		LOCAL="${DIR::-1}"
 		echo "====== $DIR ======"
