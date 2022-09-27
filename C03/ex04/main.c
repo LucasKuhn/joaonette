ad102d299a50ebd06ca55bb872c1f6e25dfa6157
Lucas Kuhn <lucasalexkuhn@gmail.com>
2022-09-27 15:32:40 +0200

    make ex04 test more readable

--- C03/ex04/main.c
+++ C03/ex04/main.c
@@ -8,17 +8,20 @@ char	*ft_strstr(char *str, char *to_find);
 int	main(void)
 {
 	char str[] = "123456";
-	printf("str: %s\n", str);
+	printf("str: %s\n\n", str);
 
-	char to_find[] = "45";
-	printf("to find: %s\n", to_find);
-	printf("ft_strstr = %s | strstr = %s\n", ft_strstr(str, to_find), strstr(str, to_find));
-
+	char to_find1[] = "45";
 	char to_find2[] = "";
-	printf("to find: %s\n", to_find2);
-	printf("ft_strstr = %s | strstr = %s\n", ft_strstr(str, to_find2), strstr(str, to_find2));
+	char to_find3[] = "Hello";
 
-	char to_find3[] = "¯\\_(ツ)_/¯";
-	printf("to find: %s\n", to_find3);
-	printf("ft_strstr = %s | strstr = %s\n", ft_strstr(str, to_find3), strstr(str, to_find3));
+	// original version
+	printf("og_strstr(\"%s\") = %s\n", to_find1, strstr(str, to_find1));
+	printf("og_strstr(\"%s\") = %s\n", to_find2, strstr(str, to_find2));
+	printf("og_strstr(\"%s\") = %s\n", to_find3, strstr(str, to_find3));
+
+	printf("\n");
+	// 42 version
+	printf("ft_strstr(\"%s\") = %s\n", to_find1, ft_strstr(str, to_find1));
+	printf("ft_strstr(\"%s\") = %s\n", to_find2, ft_strstr(str, to_find2));
+	printf("ft_strstr(\"%s\") = %s\n", to_find3, ft_strstr(str, to_find3));
 }
